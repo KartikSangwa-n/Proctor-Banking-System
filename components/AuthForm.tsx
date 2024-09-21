c42e37328cf6a33cafeee2005cc84981f576397c
Kartik Sangwan <kartiksangwan03.com>
2024-09-21 11:50:47 +0530

    Implement PlaidLinkand Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 
 const AuthForm = ({type}: {type: string}) => {
@@ -48,8 +49,21 @@ const AuthForm = ({type}: {type: string}) => {
         
         try{            
             // Sign up with Appwrite & create plaid token   
+            
             if(type == 'sign-up'){
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password,
+                }
+                const newUser = await signUp(userData);
 
                 setUser(newUser);
 
@@ -110,7 +124,7 @@ const AuthForm = ({type}: {type: string}) => {
         </header>
         {user?(
             <div className = "flex flex-col gap-4">
-                {/*Plaid Link*/}           
+                <PlaidLink user = {user} variant = "primary" />           
 
             </div>
         ):(
@@ -165,7 +179,7 @@ const AuthForm = ({type}: {type: string}) => {
                         : "Already have an assount?"}
                     </p>
                     <Link href = {type === "sign-in" ? "/sign-up": "/sign-in"} className='form-link'>
-                        {type === "sign-in" ? "/Sign Up": "/Sign In"}
+                        {type === "sign-in" ? "Sign up": "Sign in"}
                     </Link>
                 </footer>
             </>
